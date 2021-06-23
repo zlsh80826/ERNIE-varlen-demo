fbbce749d0c239c4605a98b506f210db5b0221af
zlsh80826 <zlsh80826@gmail.com>
2021-06-23 11:46:50 +0800

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -xe
 
-GPU_MODEL=`nvidia-smi -L | cut -d' ' -f4`
+GPU_MODEL=`nvidia-smi -L | cut -d: -f2 | cut -d'(' -f1 | xargs | sed -e 's/ /-/g'`
+
+if nvidia-smi -L | grep MIG > /dev/null; then
+    GPU_MODEL=${GPU_MODEL}-MIG
+fi
 
 # benchmark dense
 python3 scripts/cbenchmark.py -d QNLI -m large -i trt-fp16 -b {1,2,4,8,16,32,64,128,256} --stats_csv logs/${GPU_MODEL}-large-dense.csv
