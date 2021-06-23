edce27b3332839ef09fa8a06f012cf2e27215a4e
zlsh80826 <zlsh80826@gmail.com>
2021-06-23 02:58:23 +0000

    update benchmark scripts

---
+++ benchmark.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -xe
+
+GPU_MODEL=`nvidia-smi -L | cut -d' ' -f4`
+
+# benchmark dense
+python3 scripts/cbenchmark.py -d QNLI -m large -i trt-fp16 -b {1,2,4,8,16,32,64,128,256} --stats_csv ${GPU_MODEL}-large-dense.csv
+
+# benchmark sparse weight
+python3 scripts/cbenchmark.py -d QNLI -m large -i trt-fp16 -b {1,2,4,8,16,32,64,128,256} --sparse 1 --stats_csv ${GPU_MODEL}-large-sparse.csv
--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=/home/rewang/tools/cuda-11.0/TensorRT-7.2.1.3/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/rewang/tools/cuda-11.0/cudnn-8.0/lib64:$LD_LIBRARY_PATH
-export CUDA_VISIBLE_DEVICES=0
-
-set -xe
-
-python scripts/cbenchmark.py -d QNLI -m base -i trt-fp16 -b {16,32,64,128}
